08f74399cd0ebaa65fdc375cbc7ad376c373dd90
Rémi Palandri <palandrir@gmail.com>
2012-05-29 19:03:13 +0200

    add du projet, bitches

---
+++ JEEJPA/src/Joueur.java
@@ -0,0 +1,21 @@
+
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Joueur
+ *
+ */
+@Entity
+
+public class Joueur implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Joueur() {
+		super();
+	}
+   
+}
