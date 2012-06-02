09043505ee999bd5dfa1ae99fc6519734f8fe6c6
Marine Lavaux <lavaux.marine@gmail.com>
2012-06-02 18:52:55 +0200

    fin de partie

--- JEEJPA/src/Partie.java
+++ JEEJPA/src/Partie.java
@@ -10,13 +10,15 @@ public class Partie implements Serializable {
 
 	@Column(unique=true, nullable=false)
 	@Id
-	private int partieId;
+	private Integer partieId;
 	
 	@Column(nullable=false)
 	private Joueur joueur1;	
 	
 	@Column(nullable=false)
 	private Joueur joueur2;
+	
+	private Joueur winner;
 	
 	@OneToMany
 	private List<Coup> coup;	
@@ -25,6 +27,63 @@ public class Partie implements Serializable {
 		super();
 	}
 	
-	
+	public Partie(int partieId, Joueur joueur1, Joueur joueur2) {
+		this.partieId = partieId;
+		this.joueur1 = joueur1;
+		this.joueur2 = joueur2;
+	}
    
+	public Integer getPartieId() {
+		return this.partieId;
+	}
+	
+	public void setPartieId(Integer partieId) {
+		this.partieId = partieId;
+	}
+	
+	public Joueur getJoueur1() {
+		return this.joueur1;
+	}
+	
+	public void setJoueur1(Joueur joueur1) {
+		this.joueur1 = joueur1;
+	}
+	
+	public Joueur getJoueur2() {
+		return this.joueur1;
+	}
+	
+	public void setJoueur2(Joueur joueur2) {
+		this.joueur2 = joueur2;
+	}
+	
+	public Joueur getWinner() {
+		return this.winner;
+	}
+	
+	public void setWinner(Joueur winner) {
+		this.winner = winner;
+	}
+	
+	public List<Coup> getCoup() {
+		return this.coup;
+	}
+	
+	public void addCout(Coup coup) {
+		this.coup.add(coup);
+	}
+	
+	@Override
+	public String toString() {
+		return this.partieId.toString();
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (!(other instanceof Partie)) {
+			return false;
+		}
+		return this.getPartieId().equals(((Partie) other).getPartieId());
+	}
+	
 }
